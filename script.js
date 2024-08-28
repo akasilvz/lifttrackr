007453bb9295e5b34e532092dbe0a1f3713bd777
João Silva <joaoc_silva18@hotmail.com>
2024-08-28 10:17:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,20 +12,12 @@ function populateExerciseOptions(selectElement, onlyRecorded = false) {
 function addRow() {
     const logBody = document.getElementById('log-body');
     logBody.innerHTML = `
-<div class="input-group">
-    <div class="input-icon">
-        <i class="fas fa-calendar-alt"></i>
-        <input
-            type="text"
-            id="dateInput"
-            placeholder="Date"
-            maxlength="10"
-            required
-            onfocus="(this.type='date')"
-            onblur="(this.type='text')"
-        >
-    </div>
-</div>
+        <div class="input-group">
+            <div class="input-icon">
+                <i class="fas fa-calendar-alt"></i>
+                <input type="text" id="dateInput" placeholder="Date" maxlength="10" required>
+            </div>
+        </div>
         <div class="input-group">
             <div class="input-icon">
                 <i class="fas fa-dumbbell"></i>
@@ -54,24 +46,20 @@ function addRow() {
 
     logBody.querySelector('.validate-btn').addEventListener('click', validateRow);
 
-    const dateInput = document.getElementById('dateInput');
-
-    // Inicializar flatpickr no input de data
+    const dateInput = logBody.querySelector('#dateInput');
     flatpickr(dateInput, {
         dateFormat: "d/m/Y",
         allowInput: true,
-        onOpen: function() {
-            dateInput.type = 'date'; // Mudar para tipo 'date' ao abrir
+        onOpen: function(selectedDates, dateStr, instance) {
+            dateInput.placeholder = 'dd/mm/aaaa'; 
         },
-        onClose: function() {
-            dateInput.type = 'text'; // Voltar para tipo 'text' ao fechar
+        onClose: function(selectedDates, dateStr, instance) {
             if (!dateInput.value) {
-                dateInput.placeholder = 'Date'; // Restaurar placeholder se vazio
+                dateInput.placeholder = 'Date';
             }
         }
     });
-    
-    // Listener para formatação manual do input de data
+
     dateInput.addEventListener('input', formatDateInput);
 }
 

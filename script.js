996140ed9312aeea9b83c781458dc279bdf45e5b
João Silva <joaoc_silva18@hotmail.com>
2024-08-28 17:31:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function addRow() {
         <div class="input-group">
             <div class="input-icon">
                 <i class="fas fa-calendar-alt"></i>
-                <input type="text" id="dateInput" placeholder="Date" maxlength="10" required>
+                <input type="text" id="dateInput" placeholder="Date" maxlength="10" required onfocus="this.type='date'" onblur="this.type='text'">
             </div>
         </div>
         <div class="input-group">
